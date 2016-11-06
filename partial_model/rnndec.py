ef12b6afe9f3d37e30f4dbecbb3fec7b530ee463
eiji mishiro <eiji346g@gmail.com>
2016-11-06 21:30:33 +0900

    Issue#13 generic code for array: nice as for cpu

--- partial_model/rnndec.py
+++ partial_model/rnndec.py
@@ -9,11 +9,12 @@ import chainer.functions as F
 import chainer.links as L
 import l_maxout as lm
 import lstm as ll
+from chainer import cuda
 from chainer.functions.activation import softmax
 from ipdb import set_trace
 
 class RNNDecoder(chainer.Chain):
-  def __init__(self, target_vocab, emb_units, n_units, batchsize, train=True):
+  def __init__(self, target_vocab, emb_units, n_units, batchsize, gpu, train=True):
     super(RNNDecoder, self).__init__(
       embed = L.EmbedID(target_vocab, emb_units),
       l1 = ll.LSTMDec(emb_units, n_units),
@@ -24,10 +25,16 @@ class RNNDecoder(chainer.Chain):
       self.train = train
     self.emb_units = emb_units
     self.batchsize = batchsize
+    self.gpu = gpu
 
   def reset_state(self):
     self.l1.reset_state()
-    self.prev_y_cswr = np.zeros((self.batchsize, self.emb_units), dtype=np.float32)
+    #TODO need reset_state() about linear of maxout ソモソモ stateとlinearの違い...
+
+    first_emb = np.zeros((self.batchsize, self.emb_units), dtype=np.float32)
+    if self.gpu >= 0:
+        first_emb = cuda.to_gpu(first_emb, device=self.gpu)
+    self.prev_y_cswr = first_emb
 
   def set_l1(self, middle):
     self.l1.set_state(middle.dec_h0, middle.mid_c)
@@ -36,7 +43,6 @@ class RNNDecoder(chainer.Chain):
     self.prev_y_cswr = self.embed(prev_y_id)
 
   #management hidden state h and c in l1 not in this object
-  #TODO I don't need concatinate? reason why for GPU because different length is not accceptable in GPU unit
   def __call__(self, prev_y_ids, middle):
     batch = prev_y_ids.shape[0]
     h = self.l1(F.dropout(self.prev_y_cswr[:batch], train=self.train))
