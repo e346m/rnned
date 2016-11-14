7120bbf33bef0541b2f79b50afa438ab41c4208c
eiji mishiro <eiji346g@gmail.com>
2016-11-15 00:13:58 +0900

    plot

---
+++ study.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import seaborn as sns
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', '-d', help='Target file path')
+args = parser.parse_args()
+
+with open(args.dir, "r") as f:
+  loss = pickle.load(f)
+
+plt.plot(loss, loss, "bs")
+plt.show()
