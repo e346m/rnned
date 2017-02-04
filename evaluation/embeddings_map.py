1f45477779d59e3b646e1d12bff5d83ae292675b
eiji mishiro <eiji346g@gmail.com>
2017-02-04 21:07:33 +0900

    add pca to evaluation tool

--- evaluation/embeddings_map.py
+++ evaluation/embeddings_map.py
@@ -13,11 +13,16 @@ mpl.rcParams["font.family"] = 'Source Han Code JP'
 parser = argparse.ArgumentParser()
 parser.add_argument('--word_representaion', '-wr',
                     help="Datasets of word representaions")
+parser.add_argument('--tsne', '-tsne', action='store_true',
+                    help="choose tsne visualization")
+parser.add_argument('--pca', '-pca', action='store_true',
+                    help="choose pca visualization")
 args = parser.parse_args()
 
 with open(args.word_representaion, "rb") as f:
     rep = pickle.load(f)
 sX = [embeddings.data for embeddings in rep.values()]
+keys = rep.keys()
 
 # TEST
 #with open(args.word_vocab, "rb") as f:
@@ -28,15 +33,34 @@ sX = [embeddings.data for embeddings in rep.values()]
 ##vocab = [vocab[i] for i in vocab]
 #vocab = vacab.values
 
-model = TSNE(n_components=2,
-             perplexity=50, n_iter=500, verbose=3, random_state=1)
-sX = model.fit_transform(sX)
 
+class t_sne:
+    def __init__(self):
+        self.model = TSNE(n_components=2,
+                          perplexity=50, n_iter=500, verbose=3, random_state=1)
+
+    def transform(self, sX):
+        return self.model.fit_transform(sX)
+
+
+class pca:
+    def __init__(self):
+        self.model = PCA(n_components=2)
+
+    def transform(self, sX):
+        return self.model.fit_transform(sX)
+
+if args.pca:
+    reduction = pca()
+else:
+    reduction = t_sne()
+
+sX = reduction.transform(sX)
 plt.figure(2, figsize=(4, 3))
 plt.clf()
 plt.scatter(sX[:, 0], sX[:, 1], c="red", cmap=plt.cm.Paired)
 
-for v, s in zip(rep.keys, sX):
+for v, s in zip(keys, sX):
     plt.annotate(v, s, fontsize=10)
 
 plt.show()
